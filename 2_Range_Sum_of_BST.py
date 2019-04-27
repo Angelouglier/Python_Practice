9c9e8603f425b886f7cefa6ec027c906f28245ff
Angelo Huang <39223845+Angelouglier@users.noreply.github.com>
2019-04-28 01:48:43 +0800

    Create 2_Range_Sum_of_BST.py

---
+++ 2_Range_Sum_of_BST.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def rangeSumBST(self, root, L, R):
+        count = 0
+        if root.val >= L:
+            if root.val <= R:
+                count += root.val
+        if root.left != None:
+            count += self.rangeSumBST(root.left, L, R)
+        if root.right != None:
+            count += self.rangeSumBST(root.right, L, R)
+        return count
