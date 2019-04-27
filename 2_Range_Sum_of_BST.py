d0fd0a24934c8a561127d9ef9c1ba28b34ed2825
Angelo Huang <39223845+Angelouglier@users.noreply.github.com>
2019-04-28 02:09:20 +0800

    Update 2_Range_Sum_of_BST.py

--- 2_Range_Sum_of_BST.py
+++ 2_Range_Sum_of_BST.py
@@ -7,6 +7,12 @@
 
 class Solution(object):
     def rangeSumBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: int
+        """
         count = 0
         if root.val >= L:
             if root.val <= R:
